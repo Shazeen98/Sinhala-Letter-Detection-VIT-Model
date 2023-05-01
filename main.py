051b0a28fda47700ddee673b3598fef3fbabff4b
Mohamed Shazeen <62658770+Shazeen98@users.noreply.github.com>
2023-05-01 14:06:56 +0530

    feat: Update model

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import re
+import json
 import random
+import torch
 
 from PIL import Image
 from flask import Flask, request
@@ -7,44 +10,81 @@ from flask_cors import CORS
 from model import LetterDetection
 from db import init_db, Word
 
+from transformers import ViTModel, ViTConfig
+from torchvision.models.resnet import resnet18
+
+# from ultralytics import YOLO
+
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
 CORS(app)
 init_db(app)
 
-letter_detection = LetterDetection("./model/model_best.pth", "./model/id2letter.json", "./model/letter2id.json")
+
+class ViT(torch.nn.Module):
+
+    def __init__(self, config=ViTConfig(), num_labels=20,
+                 model_checkpoint='google/vit-base-patch16-224-in21k'):
+        super(ViT, self).__init__()
+
+        self.vit = ViTModel.from_pretrained(model_checkpoint, add_pooling_layer=False)
+        self.classifier = (
+            torch.nn.Linear(config.hidden_size, num_labels)
+        )
+
+    def forward(self, x):
+        x = self.vit(x)['last_hidden_state']
+        # Use the embedding of [CLS] token
+        output = self.classifier(x[:, 0, :])
+
+        return output
+
+
+letter_detection = LetterDetection(id2label="./model/id2letter.json", label2id="./model/letter2id.json")
+
 
 @app.route("/suggest_word", methods=['POST'])
 def get_word():
     data = request.form
-    
+
     if "image" in request.files:
-        image = Image.open(request.files.getlist()[0])
-        word = list(data["word"])
-
-        detected = letter_detection.detect(image)
+        word_orig = re.findall(r'\w\W?', data["word"])
+        images = request.files.getlist("image")
         suggested = []
-        for letter_orig, letter_detect in zip(word, detected):
-            if not (letter_orig == letter_detect):
-                words = list(Word.objects(word__contains=letter_detect))
-                freqs = []
-                for word in words:
-                    freqs.append(word.freq[letter_detect])
 
-                suggested.append(words[freqs.index(max(freqs))].word)
+        with open("predictions/predictions.jsonl", "a+") as preds:
+            for i, (letter_orig, image) in enumerate(zip(word_orig, images)):
+                image = Image.open(image).convert("RGB").resize((80, 80))
 
-        if len(suggested) >= 1:
-            return random.choice(suggested)
-        else:
-            return suggested[0]
+                # detected = letter_detection.detect(image, word)
+                detected, conf = letter_detection._classify(image)
+
+                pred = {
+                    "actual": letter_orig,
+                    "predicted": detected,
+                    "conf": conf
+                }
+
+                preds.write(json.dumps(pred) + "\n")
+
+                if not (letter_orig == detected):
+                    words = list(Word.objects(word__contains=letter_orig))
+                    freqs = []
+                    for word in words:
+                        freqs.append(word.freq[letter_orig])
+
+                    suggested.append(words[freqs.index(max(freqs))].word)
+
+            if len(suggested):
+                return re.findall(r'\w\W?', random.choice(suggested))
+            else:
+                return re.findall(r'\w\W?', random.choice(list(Word.objects(word__contains="අ"))).word)
 
     else:
-        return random.choice(list(Word.objects(word__contains="අ"))).word
-    
+
+        return re.findall(r'\w\W?', random.choice(list(Word.objects(word__contains="අ"))).word)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
-        
--- model.py
+++ model.py
@@ -6,31 +6,35 @@ import imutils
 import numpy as np
 
 from PIL import Image
+from imutils import contours
 from torchvision import transforms
-from torchvision.models.resnet import resnet50
 
 
-def union(a,b):
+def union(a, b):
     x = min(a[0], b[0])
     y = min(a[1], b[1])
-    w = max(a[0]+a[2], b[0]+b[2]) - x
-    h = max(a[1]+a[3], b[1]+b[3]) - y
+    w = max(a[0] + a[2], b[0] + b[2]) - x
+    h = max(a[1] + a[3], b[1] + b[3]) - y
     return [x, y, w, h]
 
-def _intersect(a,b):
+
+def _intersect(a, b):
     x = max(a[0], b[0])
     y = max(a[1], b[1])
-    w = min(a[0]+a[2], b[0]+b[2]) - x
-    h = min(a[1]+a[3], b[1]+b[3]) - y
-    if  w < 0 or h < 0:
+    w = min(a[0] + a[2], b[0] + b[2]) - x
+    h = min(a[1] + a[3], b[1] + b[3]) - y
+    if w < 0 or h < 0:
         return False
     return True
 
+
 def _is_near(a, b):
-    if (abs(a[0] - b[0]) < 10 and abs((a[3] + a[1]) - (b[1])) < 10) or (abs(a[1] - b[1]) < 10 and abs((a[2] + a[0]) - (b[0])) < 10):
+    if (abs(a[0] - b[0]) < 10 and abs((a[3] + a[1]) - (b[1])) < 10) or (
+            abs(a[1] - b[1]) < 10 and abs((a[2] + a[0]) - (b[0])) < 10):
         return True
     else:
         return False
+
 
 def merge(a, b):
     # if (abs(a[0] - b[0]) < 10 and abs((a[3] + a[1]) - (b[1])) < 10):
@@ -57,7 +61,7 @@ def _group_rectangles(rec):
     i = 0
     while i < len(rec):
         if not tested[i]:
-            j = i+1
+            j = i + 1
             while j < len(rec):
                 if not tested[j] and _intersect(rec[i], rec[j]):
                     rec[i] = union(rec[i], rec[j])
@@ -75,8 +79,11 @@ def _group_rectangles(rec):
 
 
 class LetterDetection():
-    def __init__(self, model_path="./model_best_resnet34.pth", id2label="./id2label.json", label2id="./label2id.json") -> None:
-        self.model = model_path
+    def __init__(self, model_path="./model/latest.pth", id2label="./model/id2label.json",
+                 label2id="./model/label2id.json") -> None:
+
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = self.load_model(model_path)
 
         with open(id2label, 'r') as idlabel:
             _id2label = json.load(idlabel)
@@ -92,21 +99,42 @@ class LetterDetection():
         for key, value in _label2id.items():
             self.label2id[key] = int(value)
 
-        self.preprocess = transforms.Compose([
-            transforms.Resize((224, 224)),
+        self.transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            # transforms.Resize((224, 80), antialias=True),
+            transforms.Resize((224, 224), antialias=True),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                 std=[0.5, 0.5, 0.5])
         ])
 
     def load_model(self, path):
-        model = resnet50()
-        model.load_state_dict(torch.load(path))
-        return model
+        # model = resnet18()
+        # done = False
+        # in_features = model.fc.in_features
+        #
+        # fc_layers = []
+        # while not done:
+        #     if (in_features / 32) < 20:
+        #         fc_layers.append(torch.nn.Linear(in_features=in_features, out_features=20, bias=True))
+        #         print("Finishing up...")
+        #         break
+        #     else:
+        #         out_features = int(in_features / 32)
+        #         print(in_features, out_features)
+        #         fc_layers.append(torch.nn.Linear(in_features=in_features, out_features=out_features, bias=True))
+        #         in_features = out_features
+        #
+        # model.fc = torch.nn.Sequential(*fc_layers)
+        # # model = HandWrittenChar()
+        # # model = torch.load(path, map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+        # model.load_state_dict(torch.load(path, map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu')))
 
-    def detect(self, image):
-        if isinstance(image, Image):
+        return torch.load(path, map_location=self.device)
+
+    def detect(self, image, word):
+        if isinstance(image, Image.Image):
             image = np.asarray(image)
-        
+
         img_gray = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2GRAY)
         img_gray = 255 - img_gray
         ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
@@ -118,52 +146,61 @@ class LetterDetection():
 
         height, width, _ = image.shape
 
-        word = []
-        for i, bbox in enumerate(boundingBoxes):
-            x, y, w, h = bbox
-            if x - 10 >= 0:
-                x = x - 5
-            
-            if y - 10 >= 0:
-                y = y - 5
+        with open("predictions/predictions.jsonl", "a+") as preds:
 
-            if w + 10 < width:
-                w = w + 10
-            else:
-                w = w + 5
+            letters = []
+            for i, bbox in enumerate(boundingBoxes):
+                x, y, w, h = bbox
+                if x - 10 >= 0:
+                    x = x - 5
 
-            if y + h + 10 < height:
-                h = h + 10
-            else:
-                h = h + 5
+                if y - 10 >= 0:
+                    y = y - 5
 
-            letter = image[y: y+h, x:x+w]
-            word.append(letter[0])
+                if w + 10 < width:
+                    w = w + 10
+                else:
+                    w = w + 5
 
+                if y + h + 10 < height:
+                    h = h + 10
+                else:
+                    h = h + 5
+
+                letter = image[y: y + h, x:x + w]
+                letter = self._classify(letter)
+                letters.append(letter[0])
+                pred = {
+                    "actual": word[i],
+                    "predicted": letter[0],
+                    "conf": letter[1]
+                }
+                preds.write(json.dumps(pred) + "\n")
+
+        return letters
 
     def _classify(self, image):
 
         if isinstance(image, np.ndarray):
-            image = self.preprocess(Image.fromarray(image))
+            image = Image.fromarray(image)
 
-        elif isinstance(image, list):
-            image = torch.tensor(image)
+        image = self.transform(image).to(self.device)
 
         if image.ndim == 3:
             image = image.unsqueeze(dim=0)
 
-        preds = self.model(image)
+        preds = torch.softmax(self.model(image), dim=1)
 
         if preds.ndim == 2:
             class_ids = torch.argmax(preds, dim=1)
             classes = ""
+            confs = []
             for i, class_id in enumerate(class_ids):
-                classes = classes + self.id2label[class_id]
+                classes = classes + self.id2label[class_id.item()]
+                confs.append(torch.max(preds).item())
 
-            return classes
+            return classes, confs
 
         else:
             class_id = torch.argmax(preds)
-            return {0: self.id2label[class_id]}
-
-        
+            return self.id2label[class_id.item()], [torch.max(preds).item()]
